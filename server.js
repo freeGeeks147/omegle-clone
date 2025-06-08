dd91226617eec19f2053d284ef65e417906edcf0
freeGeeks147 <anshuanamika44@gmail.com>
2025-06-08 18:01:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const io = new Server(server);
 let waiting = null;
 
 io.on('connection', socket => {
+  // pair logic
   if (waiting) {
     socket.partner = waiting;
     waiting.partner = socket;
+    // start chat & video
     waiting.emit('start', { initiator: false });
     socket.emit('start', { initiator: true });
     waiting = null;
@@ -19,15 +21,15 @@ io.on('connection', socket => {
     waiting = socket;
     socket.emit('waiting');
   }
-
+  // signaling
   socket.on('signal', data => {
     if (socket.partner) socket.partner.emit('signal', data);
   });
-
+  // text messages
   socket.on('message', msg => {
     if (socket.partner) socket.partner.emit('message', msg);
   });
-
+  // disconnect
   socket.on('disconnect', () => {
     if (socket.partner) socket.partner.emit('partner-disconnected');
     if (waiting === socket) waiting = null;
@@ -35,4 +37,4 @@ io.on('connection', socket => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => console.log('Server up'));
